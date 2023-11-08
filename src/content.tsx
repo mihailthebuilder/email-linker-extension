6341491f50eabbcd294405acdaad2ceed75f0c47
mmarian <mihailmarian12@gmail.com>
2023-11-08 20:37:15 +0000

    refactor content script

--- src/content.tsx
+++ src/content.tsx
@@ -1,15 +1,44 @@
-console.log("content scripts activated")
+document.addEventListener("keydown", async (event: KeyboardEvent) => {
+  console.log("keydown")
 
-document.addEventListener("keydown", (event: KeyboardEvent) => {
-  if (event.ctrlKey && event.altKey && event.key === "l") {
-    chrome.storage.local.get(["authenticationToken"]).then((result) => {
-      const token = result["authenticationToken"] as string
-
-      if (typeof token !== "string") {
-        console.error("expected token, got: ", token)
-      }
-
-      console.log("token is ", token)
-    })
+  if (!keyboardShortcutTriggered(event)) {
+    return
   }
-});
+
+  if (!zohoMailSite(document.baseURI)) {
+    return
+  }
+
+  let token = ""
+  try {
+    token = await getAuthenticationTokenFromBrowser()
+  } catch (err) {
+    console.error("Error fetching authentication error: ", err)
+    return
+  }
+
+  showNotification(`token is ${token}`)
+})
+
+function keyboardShortcutTriggered(event: KeyboardEvent): boolean {
+  return (event.ctrlKey && event.altKey && event.key === "l")
+}
+
+async function getAuthenticationTokenFromBrowser(): Promise<string> {
+  const result = await chrome.storage.local.get(["authenticationToken"])
+  const token = result["authenticationToken"] as string
+
+  if (typeof token !== "string") {
+    throw Error(`expected token, got: ${token}`)
+  }
+
+  return token
+}
+
+function zohoMailSite(url: string): boolean {
+  return false
+}
+
+function showNotification(message: string) {
+  console.log(message)
+}
