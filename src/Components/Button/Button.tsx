54103cee4af2827655e4df6d55102facebd34f95
mmarian <mihailmarian12@gmail.com>
2023-11-12 02:19:38 +0000

    Smaller font size for button and text input

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -9,7 +9,7 @@ function Button({ type, children, ...props }: Props) {
     return (
         <button
             type={type}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5 focus:outline-none focus:shadow-outline"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5 focus:outline-none focus:shadow-outline text-sm"
             {...props}
         >
             {children}
--- src/Components/TextInput/TextInput.tsx
+++ src/Components/TextInput/TextInput.tsx
@@ -5,7 +5,7 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> { }
 function TextInput({ ...props }: Props) {
     return (
         <input
-            className="mb-5 p-2 text-black border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+            className="mb-5 p-2 text-black border border-gray-300 rounded focus:outline-none focus:border-blue-500 text-sm"
             {...props}
         />
     )
