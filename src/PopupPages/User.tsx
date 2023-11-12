1897046be91ffcaee5ab69841206b4e697f747f1
mmarian <mihailmarian12@gmail.com>
2023-11-12 03:44:51 +0000

    User page styled, shows tracked link with clipboard copy

--- src/PopupPages/User.tsx
+++ src/PopupPages/User.tsx
@@ -6,61 +6,84 @@ import TextInput from '../Components/TextInput/TextInput'
 import Button from '../Components/Button/Button'
 
 function User() {
-    const { appState } = useAppState()
-    const [linkToTrack, setLinkToTrack] = useState("")
-    const [trackedLink, setTrackedLink] = useState("")
+  const { appState } = useAppState()
+  const [linkToTrack, setLinkToTrack] = useState("")
+  const [trackedLink, setTrackedLink] = useState("")
 
-    const mutation = useMutation({
-        mutationFn: () => { return fetchTrackedLink(linkToTrack, appState.authenticationToken, "") },
-        onSuccess: (trackedLink) => {
-            setTrackedLink(trackedLink)
-        }
-    })
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault()
-        mutation.mutate()
+  const mutation = useMutation({
+    mutationFn: () => { return fetchTrackedLink(linkToTrack, appState.authenticationToken, "") },
+    onSuccess: (trackedLink) => {
+      setTrackedLink(trackedLink)
     }
+  })
 
-    const onInputLinkChange = (event: React.FormEvent<HTMLInputElement>) => {
-        setLinkToTrack(event.currentTarget.value)
-        setTrackedLink("")
-    }
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    mutation.mutate()
+  }
 
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="link">Link to track</label>
-                <TextInput
-                    type="text"
-                    name="link"
-                    id="link"
-                    value={linkToTrack}
-                    onChange={onInputLinkChange}
-                />
-                <Button type="submit">Get tracked link</Button>
-            </form>
-            {trackedLink.length > 0 && <div>Tracked link is {trackedLink}</div>}
-        </>
+  const onInputLinkChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setLinkToTrack(event.currentTarget.value)
+    setTrackedLink("")
+  }
 
-    )
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="link">Link to track</label>
+        <TextInput
+          type="text"
+          name="link"
+          id="link"
+          value={linkToTrack}
+          onChange={onInputLinkChange}
+        />
+        <Button type="submit">Get tracked link</Button>
+      </form>
+      {trackedLink.length > 0 && <TrackedLink link={trackedLink} />}
+    </>
+
+  )
+}
+
+function TrackedLink({ link }: { link: string }) {
+
+  const [linkCopied, setLinkCopied] = useState(false)
+
+  const copyLinkToClipBoard = () => {
+    navigator.clipboard.writeText(link)
+    setLinkCopied(true)
+  }
+
+  return (
+    <>
+      <div>Tracked link (click to copy to clipboard)</div>
+      <div
+        className="bg-gray-800 p-1 rounded shadow-md text-white cursor-pointer"
+        onClick={copyLinkToClipBoard}
+      >
+        {link}
+      </div>
+      {linkCopied && <div>Copied!</div>}
+    </>
+  )
 }
 
 async function fetchTrackedLink(url: string, authenticationToken: string, emailSubject: string) {
-    const response = await fetch(import.meta.env.VITE_API_URL + "/user/tracklink", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${authenticationToken}`
-        },
-        body: JSON.stringify({ url, emailSubject }),
-    });
+  const response = await fetch(import.meta.env.VITE_API_URL + "/user/tracklink", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${authenticationToken}`
+    },
+    body: JSON.stringify({ url, emailSubject }),
+  });
 
-    if (!response.ok) {
-        throw new Error("Failed to register")
-    }
+  if (!response.ok) {
+    throw new Error("Failed to register")
+  }
 
-    return response.text()
+  return response.text()
 }
 
 export default User
