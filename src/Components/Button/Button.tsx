ba95d2002dc1ff0c569bc2aa8b4cb45515d5594a
mmarian <mihailmarian12@gmail.com>
2023-11-12 09:51:36 +0000

    Button is highlighted when you tab to it

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -9,7 +9,7 @@ function Button({ type, children, ...props }: Props) {
     return (
         <button
             type={type}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5 focus:outline-none focus:shadow-outline text-sm"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5 focus:outline-none focus:shadow-outline text-sm focus:bg-blue-700"
             {...props}
         >
             {children}
