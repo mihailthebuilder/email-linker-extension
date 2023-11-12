9f41301f32ede1c990f0ea1d1424e6e64c129b27
mmarian <mihailmarian12@gmail.com>
2023-11-12 01:24:36 +0000

    Register popup page styling

--- src/PopupPages/Register.tsx
+++ src/PopupPages/Register.tsx
@@ -5,6 +5,9 @@ import {
   useMutation,
 } from 'react-query'
 import { useAppState } from '../Contexts'
+import TextInput from '../Components/TextInput/TextInput'
+import Button from '../Components/Button/Button'
+
 
 function Register() {
   const [email, setEmail] = useState("")
@@ -20,43 +23,47 @@ function Register() {
   const mutation = useMutation({
     mutationFn: () => { return register(email, password) },
     onSuccess: () => {
-      setAppState(appState.withNotification("registration successful. please verify your email").withPage(Page.Login).createNewState())
+      setAppState(
+        appState
+          .withNotification("You'll receive an email in the next 15 minutes to verify your account. You can log in once the account has been verified.")
+          .withPage(Page.Login)
+          .createNewState()
+      )
     }
   })
 
   return (
     <>
-      <h1>Register</h1>
+      <h1 className="text-3xl font-bold mb-10">Register</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="email">email</label>
-          <input
+          <TextInput
             type="email"
-            name="email"
-            id="email"
             value={email}
             onChange={(e: ChangeEvent<HTMLInputElement>) => { setEmail(e.target.value) }}
           />
         </div>
 
         <div>
-          <label htmlFor="password">password</label>
-          <input
+          <TextInput
             type="password"
-            name="password"
-            id="password"
             value={password}
             onChange={(e: ChangeEvent<HTMLInputElement>) => { setPassword(e.target.value) }}
           />
         </div>
-        <button type="submit">Submit</button>
-      </form>
 
-      <div>
-        <button onClick={() => setAppState(appState.withPage(Page.Login))}>
-          Go to log in
-        </button>
-      </div>
+        <div className='mt-5'>
+          <Button type="submit">Register</Button>
+        </div>
+      </form>
+      <Button
+        type="button"
+        onClick={() => {
+          setAppState(appState.withPage(Page.Login).createNewState())
+        }}
+      >
+        Go to register
+      </Button>
     </>
   )
 }
